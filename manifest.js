133b270332020cbca723e12bcf34e703466a8d81
github.net <muacomalaquias3@gmail.com>
2025-04-18 02:43:58 +0100

    manifest.js
    Signed-off-by: github.net <muacomalaquias3@gmail.com>

---
+++ manifest.js
@@ -0,0 +1,15 @@
+{
+  "name": "Chatão",
+  "short_name": "Chatão",
+  "start_url": "chat%20ao.html",
+  "display": "standalone",
+  "background_color": "#000000",
+  "description": "Chat que funciona sem internet nem custo de dados",
+  "icons": [
+    {
+      "src": "icon.png",
+      "type": "image/png",
+      "sizes": "192x192"
+    }
+  ]
+}
